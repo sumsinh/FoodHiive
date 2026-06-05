cf9b947a2ae986e73fd1e961ecf1cdf99a75e718
sumit sinha <sumit31aug20022gmail.com>
2026-06-05 18:31:37 +0530

    Fix database configuration for Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -96,7 +96,7 @@ import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=config("DATABASE_URL")
+        default=f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_NAME')}"
     )
 }
 
